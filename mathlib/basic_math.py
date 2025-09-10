b207590089bc4fa5c6f66cfe7365247be82e2e05
Diego Leiva <lei21752@uvg.edu.gt>
2025-09-10 13:32:30 -0600

    feat: initial functions code

---
+++ mathlib/basic_math.py
@@ -0,0 +1,24 @@
+from typing import Union
+
+Number = Union[int, float]
+
+
+def square():
+    # TODO: Implement the square function
+    pass
+
+def factorial():
+    # TODO: Implement the factorial function
+    pass
+
+def is_prime():
+    # TODO: Implement the is_prime function
+    pass
+
+def gcd():
+    # TODO: Implement the gcd function
+    pass
+
+def lcm():
+    # TODO: Implement the lcm function
+    pass
